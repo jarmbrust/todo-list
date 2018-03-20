c2d442c567e48921a2148cb8b1e1463a0e492d3c
James Armbrust <james.armbrust@gmail.com>
2018-03-20 06:44:21 -0600

    a little clearnup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <p className="title">Todo List</p>
-          <TodoList />
+        <TodoList />
       </div>
     );
   }
